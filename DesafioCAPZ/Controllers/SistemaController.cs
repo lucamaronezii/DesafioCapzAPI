a36e169f2e848b141442e6e91b7175377274000e
Luca <steyerluca@hotmail.com>
2023-09-12 17:28:30 -0300

    Desenvolvimento API

---
+++ DesafioCAPZ/Controllers/SistemaController.cs
@@ -0,0 +1,42 @@
+﻿using DesafioCAPZ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioCAPZ.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SistemaController : ControllerBase
+{
+
+    private static List<Dado> dados = new List<Dado>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionaDado([FromBody] Dado dado)
+    {
+        dado.Id = id++;
+        dados.Add(dado);
+        return CreatedAtAction(nameof(RecuperaDadoPorID), 
+            new {id = dado.Id}, dado);
+    } 
+
+    [HttpGet]
+    public IEnumerable<Dado> RecuperaDados([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return dados.Skip(skip).Take(take);
+    }
+
+    //[HttpGet("{id}")]
+    //public Dado? RecuperaDadoPorID(string id) 
+    //{
+    //    return dados.FirstOrDefault(dado => dado.MaterialID == id);
+    //}
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaDadoPorID(int id)
+    {
+        var dado = dados.FirstOrDefault(dado => dado.Id == id);
+        if (dado == null) return NotFound();
+        return Ok(dado);
+    }
+}
---
+++ DesafioCAPZ/Models/Dado.cs
@@ -0,0 +1,15 @@
+﻿using DesafioCAPZ.Controllers;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioCAPZ.Models;
+
+public class Dado
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O ID do material não pode ser vazio.")]
+    [MinLength(2, ErrorMessage = "É obrigatória a presença de 2 ou mais caracteres")]
+    public string MaterialID { get; set; }
+    [Required(ErrorMessage = "O nome do material não pode ser vazio.")]
+    [MinLength(2, ErrorMessage = "É obrigatória a presença de 2 ou mais caracteres")]
+    public string MaterialName { get; set; }
+}
